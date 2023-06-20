653d42add9a0c9eee14505b201ad1b65d1e31f05
Waleed Tariq <waleedtariq109@outlook.com>
2023-06-20 11:49:23 +0500

    initial

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  docus: {
+    title: "Nuxt3 - Migration Guide",
+    main: {
+      padded: true,
+      fluid: true,
+    },
+    header: {
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true,
+    },
+  },
+});
